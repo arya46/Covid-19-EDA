53eec8a2bffa2194599d4f3ce72675c2c18b9b5a
arya.46 <tulrosdeori@gmail.com>
2020-06-29 11:51:16 +0530

    fixed CI and main.sh

--- main.sh
+++ main.sh
@@ -3,7 +3,6 @@ set -eu
 repo_uri="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 remote_name="origin"
 main_branch="master"
-app_name="${HEROKU_APP_NAME}"
 
 git config user.name "$GITHUB_ACTOR"
 git config user.email "${GITHUB_ACTOR}@bots.github.com"
@@ -19,12 +18,6 @@ then
     git commit -am "data updated on - $(date)"
     git remote set-url "$remote_name" "$repo_uri"
     git push -u "$remote_name" "$main_branch"
-    echo "Pushed to Github"
-
-    heroku auth:token
-    heroku git:remote -a "$app_name"
-    git push heroku "$main_branch"
-    echo "Pushed to Heroku"
 else
     set -e
     echo "No changes since last run"
