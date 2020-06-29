dd03c0efb2dc15ff6ac580e03ed576522d31f4e3
arya.46 <tulrosdeori@gmail.com>
2020-06-29 11:41:47 +0530

    fixed the actions workflow

--- main.sh
+++ main.sh
@@ -3,9 +3,12 @@ set -eu
 repo_uri="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 remote_name="origin"
 main_branch="master"
+app_name="${HEROKU_APP_NAME}"
 
 git config user.name "$GITHUB_ACTOR"
 git config user.email "${GITHUB_ACTOR}@bots.github.com"
+
+echo "start of task"
 
 git add .
 set +e  # Grep succeeds with nonzero exit codes to show results.
@@ -14,11 +17,17 @@ if [ $? -eq 0 ]
 then
     set -e
     git commit -am "data updated on - $(date)"
-    git remote set-url "$remote_name" "$repo_uri" # includes access token
+    git remote set-url "$remote_name" "$repo_uri"
     git push -u "$remote_name" "$main_branch"
+    echo "Pushed to Github"
+
+    heroku auth:token
+    heroku git:remote -a "$app_name"
+    git push heroku "$main_branch"
+    echo "Pushed to Heroku"
 else
     set -e
     echo "No changes since last run"
 fi
 
-echo "finish"
+echo "end of task"
